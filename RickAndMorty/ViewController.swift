1ef821812c9956efddbf39ef485285d8b7e5b300
faridrustam <faridrustamov7442@gmail.com>
2025-09-10 14:02:16 +0400

    Project created

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Farid Rustamov on 10.09.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
